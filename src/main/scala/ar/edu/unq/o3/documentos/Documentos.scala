1053e0ba16e59378fdb4213800727a1db94ced52
Javier Fernandes <javier.fernandes@gmail.com>
2018-10-10 11:09:11 -0300

    Ejemplo en clase. Llegamos hasta el aggregate con match (un poco crudo). Muestra hasta pattern matching y options

--- src/main/scala/ar/edu/unq/o3/documentos/Documentos.scala
+++ src/main/scala/ar/edu/unq/o3/documentos/Documentos.scala
@@ -1,55 +1,73 @@
 package ar.edu.unq.o3.documentos {
 
-  // general types
-
+  /**
+   * Type aliases para nombrar tipos de funciones genéricas
+   */
   object Types {
-    type Filter = Field => Boolean
+    type AggregateStep = List[Document] => List[Document]
     type DocumentFilter = (Document) => Boolean
   }
 
-  object Operaciones {
-    def project(spec: String*) = (doc: Document) => doc.filter(f => spec.toList.contains(f.name))
-  }
+  /**
+    * Pasos en el aggregate
+    */
+  object AggregateSteps {
 
+    def project(fields: List[String])(docs:List[Document])=
+      docs.map(Operations.projectDocument(fields))
+
+    def matching(filter: Types.DocumentFilter)(docs:List[Document]) =
+      docs.filter(filter)
+
+    // podríamos agregar un $groupBy, $count, $limit, etc
+    // https://docs.mongodb.com/manual/reference/operator/aggregation/count/
+  }
 
   //
-  // aggregate
+  // Modelo de objetos de documentos
   //
-
-  trait AggregateStep {
-    def perform(docs: List[Document]): List[Document]
-  }
-
-  case class Project(spec: String*) extends AggregateStep {
-    import Operaciones._
-    override def perform(docs: List[Document]): List[Document] = docs.map(project(spec:_*))
-  }
-  case class Match(filter: Types.DocumentFilter) extends AggregateStep {
-    override def perform(docs: List[Document]): List[Document] = docs.filter(filter)
-  }
-
-  // Document model
 
   trait Value
-  case class Number(numero: Int) extends Value
-  case class Text(texto: String) extends Value
+
+  case class Number(number: Int) extends Value
+  case class Text(text: String) extends Value
 
   case class Field(name: String, value: Value)
-
   case class Document(fields: List[Field]) extends Value {
-    import Types._
 
-    def filter(filter: Filter) = copy(fields = fields.filter(filter))
-    def get(fieldName: String) = fields.find(_.name == fieldName)
-  }
+    def get(fieldName: String) = fields
+      .find(_.name == fieldName)
+      .map(_.value)
 
-  case class DbCollection(documents: List[Document]) {
-    import Types._
-
-    def aggregate(steps: AggregateStep*) = steps.foldLeft(documents) {
-      (docs, step) => step.perform(docs)
+    def filter(_fields: List[String]) = {
+      copy(fields = fields.filter(f => _fields.contains(f.name)))
     }
   }
+
+  case class DBCollection(documents: List[Document]) {
+    import Types._
+
+    def aggregate(steps: List[AggregateStep]) : List[Document] = steps.foldLeft(documents) {
+      (accDocs, step) => step(accDocs)
+    }
+
+  }
+
+  /**
+   * Utils para construir documentos con una sintaxis más corta
+   */
+  object ValueImplicits {
+    implicit def stringToText = Text(_)
+    implicit def intToNumber = Number(_)
+  }
+
+  /**
+   * Operaciones sobre documentos
+   */
+  object Operations {
+    def projectDocument(fields: List[String]) = (doc: Document) => doc.filter(fields)
+  }
+
 
 }
 
--- src/test/scala/DocumentosSpec.scala
+++ src/test/scala/DocumentosSpec.scala
@@ -1,116 +1,104 @@
 import org.scalatest.{FunSpec, Matchers}
 import ar.edu.unq.o3.documentos._
-import ar.edu.unq.o3.documentos.Operaciones._
+import ar.edu.unq.o3.documentos.ValueImplicits._
 
 class DocumentosSpec extends FunSpec with Matchers {
 
-  // { nombre: "Juan", apellido: "Perez", edad: 33 }
-  // { nombre: "Juan", apellido: "Perez", edad: 33 }
+  // Ejemplo de documentos de mongo
+  // { nombre: "Morty", apellido: "Sanchez", edad: 13 }
+  // { nombre: "Rick", apellido: "Sanchez", edad: 33 }
 
-  //db.collection('personas').aggregate({
-  //  { $project: { nombre: "$nombre", apellido: "$apellido" } }
-  //})
+
+  // ejemplo de consulta aggregate en mongo con 2 pasos: filtrado (match), y luego proyección
+  //db.collection('personas').aggregate([
+  //  { $match: { edad: { $gte: 18 } } },
+  //  { $project: { quien: "$nombre", edad: "$edad } }
+  //])
+  // produce
+  //  [{ quien: "Rick", edad: 13 }]
 
   describe("DocDB") {
 
     describe("project()") {
 
-      it("debe permitir filtrar") {
+      import ar.edu.unq.o3.documentos.Operations.{ projectDocument }
+
+      it("debe proyectar 1 unico field") {
+
         val juan = Document(List(
           Field("nombre", Text("Juan")),
-          Field("apellido", Text("Perez")),
+          Field("apellido", Text("Perez"))
         ))
 
-        val nombre = project("nombre")
-        nombre(juan) should equal(Document(List(Field("nombre", Text("Juan")))))
+        val proyectarNombre = projectDocument(List("nombre"))
+
+        (proyectarNombre(juan)) should equal(Document(List(
+          Field("nombre", "Juan") // los implicits nos permiten escribir "Juan" en lugar de Text("Juan")
+        )))
       }
 
-      it("debe permitir fields de tipo numero") {
+      it("debe proyectar 2 fields") {
+
         val juan = Document(List(
-          Field("nombre", Text("Juan")),
-          Field("apellido", Text("Perez")),
-          Field("edad", Number(33)),
+          Field("nombre", "Juan"),
+          Field("apellido", "Perez"),
+          Field("direccion", "Av siempre viva 1234")
         ))
 
-        val edad = project("edad")
-        edad(juan) should equal(Document(List(Field("edad", Number(33)))))
+        projectDocument(List("nombre", "apellido"))(juan) should equal(Document(List(
+          Field("nombre", "Juan"),
+          Field("apellido", "Perez"),
+        )))
       }
 
-      it("debe permitir multiples fields") {
+      it("debe proyectar 1 campo number") {
+
         val juan = Document(List(
-          Field("nombre", Text("Juan")),
-          Field("apellido", Text("Perez")),
-          Field("edad", Number(33)),
+          Field("nombre", "Juan"),
+          Field("apellido", "Perez"),
+          Field("edad", 33) // implicit acá nos permite escribir 33 en lugar de Number(33)
         ))
 
-        val nombreYEdad = project("nombre", "edad")
-        nombreYEdad(juan) should equal(Document(List(Field("nombre", Text("Juan")), Field("edad", Number(33)))))
+        projectDocument(List("edad"))(juan) should equal(Document(List(
+          Field("edad", 33),
+        )))
       }
-
-//      it("debe permitir transformar fields") {
-//        val juan = Document(List(
-//          Field("nombre", Text("Juan")),
-//          Field("apellido", Text("Perez")),
-//        ))
-//
-//        val nombre = project(Map(
-//          "nombre" -> Copy("nombre"),
-//          "apellido" -> Substr(0, 3),
-//        ))
-//        nombre(juan) should equal(Document(List(
-//          Field("nombre", Text("Juan")))),
-//          Field("apellido", Text("Per"))
-//        )
-//      }
 
     }
 
-    describe("Collection") {
+    describe("aggregate()") {
+      import ar.edu.unq.o3.documentos.AggregateSteps._
 
-      describe("aggregate()") {
+      implicit def tupleToField(t : (String, Int)) = Field(t._1, t._2)
 
-        val juan = Document(List(
-          Field("nombre", Text("Juan")),
-          Field("apellido", Text("Perez")),
-          Field("edad", Number(33)),
+      val juan = Document(List(
+        Field("nombre", Text("Juan")),
+        Field("apellido", Text("Perez")),
+        "edad" -> 17   // implicits acá nos permite escribir una Tupla(String, Int) en lugar de Field("edad", 17)
+      ))
+
+      val morty = Document(List(
+        Field("nombre", Text("Morty")),
+        Field("apellido", Text("Sanchez")),
+        "edad" -> 67
+      ))
+
+      it("should perform a single project") {
+        DBCollection(List(
+          juan,
+          morty
+        )).aggregate(List(
+          // el match está un poco "crudo" recibe una funcion (Document) => Boolean
+          // para ver si pasa. Podríamos trabajarlo para parecerse a mongo
+          matching(d => d.get("edad") match {
+            case Some(Number(edad)) if (edad >= 18) => true
+            case _ => false
+          }),
+          project(List("nombre")),
+        )) should equal(List(
+          Document(List(Field("nombre", "Morty"))),
         ))
-
-        val maria = Document(List(
-          Field("nombre", Text("Maria")),
-          Field("apellido", Text("Perez")),
-          Field("edad", Number(27)),
-        ))
-
-        it("should be able to project()") {
-          DbCollection(List(juan, maria)).aggregate(Project("edad")) should equal(List(
-            Document(List(Field("edad", Number(33)))),
-            Document(List(Field("edad", Number(27))))
-          ))
-        }
-
-        it("should be able to aggregate two projects in a row") {
-          DbCollection(List(juan, maria)).aggregate(
-            Project("nombre", "apellido"),
-            Project("nombre"),
-          ) should equal(List(
-            Document(List(Field("nombre", Text("Juan")))),
-            Document(List(Field("nombre", Text("Maria"))))
-          ))
-        }
-
-        it("should support match -> project") {
-          DbCollection(List(juan, maria)).aggregate(
-            Match(d => d.get("edad").map(_.value) match {
-              case Some(Number(n)) if (n > 30) => true
-              case _ => false
-            }),
-            Project("nombre"),
-          ) should equal(List(
-            Document(List(Field("nombre", Text("Juan")))),
-          ))
-        }
       }
-
     }
 
   }
